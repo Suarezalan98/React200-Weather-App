7a230e5bfd8ab30ee8f94d2f46a71f3bb525a0cf
Alan Suarez <Alan.Andre.Suarez@gmail.com>
2020-03-20 14:55:40 -0700

    prepared for Now deplyoment

--- src/js/components/SearchBar/SearchBarActions.js
+++ src/js/components/SearchBar/SearchBarActions.js
@@ -3,7 +3,9 @@ import axios from "axios";
 export function getWeather(location) {
   return {
     type: "GET_WEATHER_FROM_LOCATION",
-    payload: axios.get(`/search/${location}`)
+    payload: axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=cb0db5494d1b8eeb24a4924ed7d25d2f&units=imperial`
+    )
   };
 }
 
